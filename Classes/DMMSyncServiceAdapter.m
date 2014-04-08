840ab00ce96b04406fa71f80537eeb35fb4b5cf5
Delisa Mason <iskanamagus@gmail.com>
2014-04-08 14:48:16 -0700

    Update headings

--- Classes/DMMSyncServiceAdapter.m
+++ Classes/DMMSyncServiceAdapter.m
@@ -2,7 +2,7 @@
 //  DMMSyncServiceAdapter.m
 //  SimpleSyncService
 //
-//  Copyright (c) 2013 Delisa Mason. http://delisa.me
+//  Copyright (c) 2014 Delisa Mason. http://delisa.me
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
